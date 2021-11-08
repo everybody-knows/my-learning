1d5b25eae1e667ee5777fdf1ff3e7a66deec7127
Aleksandr Saltykov <A.V.Saltykov@icloud.com>
2021-11-08 22:17:49 +0300

    Homework 2.3

---
+++ iOS/Q1/2. IOS Application User Interface/Homework 2.3 Designing The Interface. Part III./Homework/FriendsTab/FriendsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsCollectionViewCell.swift
+//  Homework
+//
+//  Created by Александр on 12.03.2021.
+//
+
+import UIKit
+
+class FriendsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var friendImage: UIImageView!
+    
+}
---
+++ iOS/Q1/2. IOS Application User Interface/Homework 2.3 Designing The Interface. Part III./Homework/Model/Group.swift
@@ -0,0 +1,16 @@
+//
+//  Group.swift
+//  Homework
+//
+//  Created by Александр on 25.03.2021.
+//
+
+import Foundation
+import UIKit
+
+struct Group {
+    let avatar: UIImage
+    let name: String
+    let description: String
+    let numberOfUsers: Int
+}
